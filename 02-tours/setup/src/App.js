7d18e6a68ace9169ecaed009176c709d1d842d62
Roger <rroger22222@gmail.com>
2022-10-18 18:36:13 +1100

    02-Tours

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setloading] = useState(true);
+  const [tours, setTours] = useState([]);
+  // Need a way to delete a tour
+  const deleteTour = (id) => {
+    setTours(tours.filter((el) => el.id !== id));
+  };
+  const fetchTours = async () => {
+    setloading(true);
+    try {
+      const reponse = await fetch(url);
+      const tours = await reponse.json();
+      setloading(false);
+      setTours(tours);
+    } catch (error) {
+      setloading(false);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading></Loading>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} deleteTour={deleteTour}></Tours>
+    </main>
+  );
 }
 
-export default App
+export default App;
