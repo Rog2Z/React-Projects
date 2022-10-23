5f8b0bc6083ca01dd8a3344768175f68b1ff9aa1
Roger <rroger22222@gmail.com>
2022-10-23 19:51:09 +1100

    reviews

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
-import Review from './Review';
+import React, { useState } from "react";
+import Review from "./Review";
+import reviews from "./data";
+console.log("from app", reviews[0]);
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  const [currReview, setCurrReview] = useState(0);
+  const revLength = reviews.length;
+
+  const changeReview = (incrementBy) => {
+    // CurrReview is a number....
+    setCurrReview((currReview) => {
+      let calRev = currReview + incrementBy;
+      calRev = calRev === -1 ? revLength - 1 : calRev;
+      calRev = calRev === revLength ? 0 : calRev;
+      return calRev;
+    });
+  };
+  return (
+    <main className="container">
+      {reviews[currReview] ? (
+        <Review
+          review={reviews[currReview]}
+          changeReview={changeReview}
+        ></Review>
+      ) : null}
+    </main>
+  );
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
-const Review = () => {
-  return <h2>review component</h2>;
+const Review = ({ review, changeReview }) => {
+  const { name, image, job, text } = review;
+
+  return (
+    <div className="review">
+      <div className="img-container ">
+        <img className="person-img" src={image}></img>
+        <div className="quote-icon">"</div>
+      </div>
+      <p className="author"> {name} </p>
+      <p className="job">{job}</p>
+      <p className="info"> {text} </p>
+      <button className="next-btn" onClick={() => changeReview(-1)}>
+        &lt;
+      </button>
+      <button className="prev-btn" onClick={() => changeReview(1)}>
+        &gt;
+      </button>
+      <button className="random-btn"> I feel lucky</button>
+    </div>
+  );
 };
 
 export default Review;
