56a9b0bd2f9c8e404cb7cb6f57ca489911082e25
Roger <rroger22222@gmail.com>
2022-10-30 16:10:57 +1100

    Post-solution Review

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -7,18 +7,38 @@ function App() {
   const [currReview, setCurrReview] = useState(0);
   const revLength = reviews.length;
 
+  // Separate funciton
+  const checkNumber = (number) => {
+    number = number === -1 ? revLength - 1 : number;
+    number = number === revLength ? 0 : number;
+    return number;
+  };
+  // This can be at the review level
   const changeReview = (incrementBy) => {
     // CurrReview is a number....
     setCurrReview((currReview) => {
       let calRev = currReview + incrementBy;
-      calRev = calRev === -1 ? revLength - 1 : calRev;
-      calRev = calRev === revLength ? 0 : calRev;
-      return calRev;
+      return checkNumber(calRev);
     });
   };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * reviews.length);
+    if (randomNumber === currReview) {
+      randomNumber = currReview + 1;
+    }
+    setCurrReview(checkNumber(randomNumber));
+  };
   return (
-    <main className="container">
-      <Review review={reviews[currReview]} changeReview={changeReview}></Review>
+    <main>
+      <section className="container">
+        <div className="title">Reviews</div>
+        <div className="underline"></div>
+        <Review
+          review={reviews[currReview]}
+          changeReview={changeReview}
+          randomise={randomPerson}
+        ></Review>
+      </section>
     </main>
   );
 }
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -2,25 +2,31 @@ import React, { useState } from "react";
 import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
-const Review = ({ review, changeReview }) => {
+const Review = ({ review, changeReview, randomise }) => {
   const { name, image, job, text } = review;
 
   return (
     <div className="review">
       <div className="img-container ">
-        <img className="person-img" src={image}></img>
-        <div className="quote-icon">"</div>
+        <img className="person-img" src={image} alt={name}></img>
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
       </div>
       <p className="author"> {name} </p>
       <p className="job">{job}</p>
       <p className="info"> {text} </p>
       <button className="next-btn" onClick={() => changeReview(-1)}>
-        &lt;
+        <FaChevronLeft />
       </button>
       <button className="prev-btn" onClick={() => changeReview(1)}>
-        &gt;
+        <FaChevronRight />
       </button>
-      <button className="random-btn"> I feel lucky</button>
+      <div>
+        <button className="random-btn" onClick={() => randomise()}>
+          I feel lucky
+        </button>
+      </div>
     </div>
   );
 };
