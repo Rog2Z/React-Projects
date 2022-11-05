b33c65c5fe639787e6f014925ccabb41ec4243aa
Roger <rroger22222@gmail.com>
2022-11-06 00:00:34 +1100

    starting part 6

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  return <h2>tabs project setup</h2>
+  useEffect(async () => {
+    const response = await fetch(url);
+    const resumes = await response.json();
+    if (response.status === 200) {
+      console.log(resumes);
+    }
+  }, []);
+
+  return <h2>tabs project setup</h2>;
 }
 
-export default App
+export default App;
